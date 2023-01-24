6e26df91694dc69a6bcab1c10b71fc86d919f0b0
Rina Neman <105747673+rinagobo@users.noreply.github.com>
2023-01-23 22:03:15 -0600

    Update main.1 to be understandable.

--- TicTacToe/main_1.py
+++ TicTacToe/main_1.py
@@ -1,160 +1,135 @@
 import random
 
-# Tic Tac Toe Board
-F_LINE = [0, 1, 2]
-S_LINE = [3, 4, 5]
-T_LINE = [6, 7, 8]
+theBoard = {0:'0', 1:'1', 2:'2',
+            3:'3', 4:'4', 5:'5',
+            6:'6', 7:'7', 8:'8'}
 
-ALL_LINES = [F_LINE, S_LINE, T_LINE]
 ALL_SPOTS = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-ALL_SPOTS_LIST = [0, 1, 2, 3, 4, 5, 6, 7, 8]
 
-# Show the Board
-def show_board():
-    print(F_LINE)
-    print(S_LINE)
-    print(T_LINE)
+def printBoard(board):
+  print(board[0]+'|'+board[1]+'|'+board[2])
+  print('-+-+-')
+  print(board[3]+'|'+board[4]+'|'+board[5])
+  print('-+-+-')
+  print(board[6]+'|'+board[7]+'|'+board[8])
+
+printBoard(theBoard)
+
+
 
 def check_computer():
-    if ALL_SPOTS[0]=="❌" and ALL_SPOTS[1]=="❌" and ALL_SPOTS[2]=="❌":
+
+    if theBoard[0]=="×" and theBoard[1]=="×" and theBoard[2]=="×":
         print("You lose...")
         return False
 
-    elif ALL_SPOTS[3]=="❌" and ALL_SPOTS[4]=="❌" and ALL_SPOTS[5]=="❌":
+    elif theBoard[3]=="×" and theBoard[4]=="×" and theBoard[5]=="×":
         print("You lose...")
         return False
 
-    elif ALL_SPOTS[6]=="❌" and ALL_SPOTS[7]=="❌" and ALL_SPOTS[8]=="❌":
+    elif theBoard[6]=="×" and theBoard[7]=="×" and theBoard[8]=="×":
         print("You lose...")
         return False
 
-    elif ALL_SPOTS[0]=="❌" and ALL_SPOTS[3]=="❌" and ALL_SPOTS[6]=="❌":
+    elif theBoard[0] == "×" and theBoard[3] == "×" and theBoard[6] == "×":
         print("You lose...")
         return False
 
-    elif ALL_SPOTS[1]=="❌" and ALL_SPOTS[4]=="❌" and ALL_SPOTS[7]=="❌":
+    elif theBoard[1] == "×" and theBoard[4] == "×" and theBoard[7] == "×":
         print("You lose...")
         return False
 
-    elif ALL_SPOTS[2]=="❌" and ALL_SPOTS[5]=="❌" and ALL_SPOTS[8]=="❌":
+    elif theBoard[2] == "×" and theBoard[5] == "×" and theBoard[8] == "×":
         print("You lose...")
         return False
 
-    elif ALL_SPOTS[0]=="❌" and ALL_SPOTS[4]=="❌" and ALL_SPOTS[8]=="❌":
+    elif theBoard[0] == "×" and theBoard[4] == "×" and theBoard[8] == "×":
         print("You lose...")
         return False
 
-    elif ALL_SPOTS[2]=="❌" and ALL_SPOTS[4]=="❌" and ALL_SPOTS[6]=="❌":
+    elif theBoard[2] == "×" and theBoard[4] == "×" and theBoard[6] == "×":
         print("You lose...")
         return False
 
-    elif ALL_SPOTS_LIST == []:
+    elif theBoard[3] == "×" and theBoard[4] == "×" and theBoard[5] == "×":
+        print("You lose...")
+        return False
+
+    elif ALL_SPOTS == []:
         print("It's draw.haha")
         return False
 
+
 def check_user():
-    if ALL_SPOTS[0]=="⭕" and ALL_SPOTS[1]=="⭕" and ALL_SPOTS[2]=="⭕":
-        print("You win!!!")
+    if theBoard[0] == "○" and theBoard[1] == "○" and theBoard[2] == "○":
+        print("You win!")
         return False
 
-    elif ALL_SPOTS[3]=="⭕" and ALL_SPOTS[4]=="⭕" and ALL_SPOTS[5]=="⭕":
-        print("You win!!!")
+    elif theBoard[3] == "○" and theBoard[4] == "○" and theBoard[5] == "○":
+        print("You win!")
         return False
 
-    elif ALL_SPOTS[6]=="⭕" and ALL_SPOTS[7]=="⭕" and ALL_SPOTS[8]=="⭕":
-        print("You win!!!")
+    elif theBoard[6] == "○" and theBoard[7] == "○" and theBoard[8] == "○":
+        print("You win!")
         return False
 
-    elif ALL_SPOTS[0]=="⭕" and ALL_SPOTS[3]=="⭕" and ALL_SPOTS[6]=="⭕":
-        print("You win!!!")
+    elif theBoard[0] == "○" and theBoard[3] == "○" and theBoard[6] == "○":
+        print("You win!")
         return False
 
-    elif ALL_SPOTS[1]=="⭕" and ALL_SPOTS[4]=="⭕" and ALL_SPOTS[7]=="⭕":
-        print("You win!!!")
+    elif theBoard[1] == "○" and theBoard[4] == "○" and theBoard[7] == "○":
+        print("You win!")
         return False
 
-    elif ALL_SPOTS[2]=="⭕" and ALL_SPOTS[5]=="⭕" and ALL_SPOTS[8]=="⭕":
-        print("You win!!!")
+    elif theBoard[2] == "○" and theBoard[5] == "○" and theBoard[8] == "○":
+        print("You win!")
         return False
 
-    elif ALL_SPOTS[0]=="⭕" and ALL_SPOTS[4]=="⭕" and ALL_SPOTS[8]=="⭕":
-        print("You win!!!")
+    elif theBoard[0] == "○" and theBoard[4] == "○" and theBoard[8] == "○":
+        print("You win!")
         return False
 
-    elif ALL_SPOTS[2]=="⭕" and ALL_SPOTS[4]=="⭕" and ALL_SPOTS[6]=="⭕":
-        print("You win!!!")
+    elif theBoard[2] == "○" and theBoard[4] == "○" and theBoard[6] == "○":
+        print("You win!")
         return False
+
+    elif theBoard[3] == "○" and theBoard[4] == "○" and theBoard[5] == "○":
+        print("You win!")
+        return False
+
+    elif ALL_SPOTS == []:
+        print("It's draw.haha")
+        return False
+
+
 
 game_is_on = True
 while game_is_on:
-    # print(ALL_SPOTS_LIST)
-    # print(ALL_SPOTS)
-
     # Action of Computer
     print("\nIt's computer's turn.")
-    ran_spot = random.choice(ALL_SPOTS_LIST)
+    ran_spot = random.choice(ALL_SPOTS)
 
-    for spot in F_LINE:
-        if spot == ran_spot:
-            spot_index = F_LINE.index(spot)
-            F_LINE = F_LINE[:spot_index]+["❌"]+F_LINE[spot_index+1:]
-            # print(F_LINE)
+    theBoard[ran_spot] = "×"
 
-    for spot in S_LINE:
-        if spot == ran_spot:
-            spot_index = S_LINE.index(spot)
-            S_LINE = S_LINE[:spot_index] + ["❌"] + S_LINE[spot_index + 1:]
-            # print(S_LINE)
-
-    for spot in T_LINE:
-        if spot == ran_spot:
-            spot_index = T_LINE.index(spot)
-            T_LINE = T_LINE[:spot_index] + ["❌"] + T_LINE[spot_index + 1:]
-            # print(T_LINE)
-
-    for spot in ALL_SPOTS:
-        if spot == ran_spot:
-            spot_index = ALL_SPOTS.index(spot)
-            ALL_SPOTS = ALL_SPOTS[:spot_index] + ["❌"] + ALL_SPOTS[spot_index + 1:]
-
-    for item in ALL_SPOTS_LIST:
+    for item in ALL_SPOTS:
         if item == ran_spot:
-            ALL_SPOTS_LIST.remove(item)
+            ALL_SPOTS.remove(item)
 
-    show_board()
+    printBoard(theBoard)
+
     if check_computer() == False:
         game_is_on = False
 
     else:
-        user_choice = int(input("\nIt is your turn.\nWhich left spot do you want to put your ⭕ ? Answer the number.\n"))
+        user_choice = int(input("\nIt is your turn.\nWhich left spot do you want to put your ○ ? Answer the number.\n"))
 
-        for spot in F_LINE:
-            if spot == user_choice:
-                spot_index = F_LINE.index(spot)
-                F_LINE = F_LINE[:spot_index]+["⭕"]+F_LINE[spot_index+1:]
-                # print(F_LINE)
+        theBoard[user_choice] = "○"
 
-        for spot in S_LINE:
-            if spot == user_choice:
-                spot_index = S_LINE.index(spot)
-                S_LINE = S_LINE[:spot_index] + ["⭕"] + S_LINE[spot_index + 1:]
-                # print(S_LINE)
-
-        for spot in T_LINE:
-            if spot == user_choice:
-                spot_index = T_LINE.index(spot)
-                T_LINE = T_LINE[:spot_index] + ["⭕"] + T_LINE[spot_index + 1:]
-                # print(T_LINE)
-
-        for spot in ALL_SPOTS:
-            if spot == user_choice:
-                spot_index = ALL_SPOTS.index(spot)
-                ALL_SPOTS = ALL_SPOTS[:spot_index] + ["⭕"] + ALL_SPOTS[spot_index + 1:]
-
-        for item in ALL_SPOTS_LIST:
+        for item in ALL_SPOTS:
             if item == user_choice:
-                ALL_SPOTS_LIST.remove(item)
+                ALL_SPOTS.remove(item)
 
-        show_board()
+        printBoard(theBoard)
+
         if check_user() == False:
             game_is_on = False
