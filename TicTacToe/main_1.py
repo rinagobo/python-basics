f8ac097c47b661225ca418a6885ee65e43527dda
Rina Yamada <rinabo@RinanoMBP.attlocal.net>
2023-01-29 03:05:57 -0600

    modify main_1.py to have less code

--- TicTacToe/main_1.py
+++ TicTacToe/main_1.py
@@ -1,81 +1,86 @@
 import random
 
-theBoard = {0:'0', 1:'1', 2:'2',
-            3:'3', 4:'4', 5:'5',
-            6:'6', 7:'7', 8:'8'}
+
+
+####### Global variables for the game #######
+theBoard = {0: '0', 1: '1', 2: '2',
+            3: '3', 4: '4', 5: '5',
+            6: '6', 7: '7', 8: '8'}
 
 ALL_SPOTS = [0, 1, 2, 3, 4, 5, 6, 7, 8]
 
+
+####### Functions for the game #######
 def printBoard(board):
-  print(board[0]+'|'+board[1]+'|'+board[2])
-  print('-+-+-')
-  print(board[3]+'|'+board[4]+'|'+board[5])
-  print('-+-+-')
-  print(board[6]+'|'+board[7]+'|'+board[8])
+    print(board[0] + '|' + board[1] + '|' + board[2])
+    print('-+-+-')
+    print(board[3] + '|' + board[4] + '|' + board[5])
+    print('-+-+-')
+    print(board[6] + '|' + board[7] + '|' + board[8])
 
-printBoard(theBoard)
+def modify_board(spot, all_spots, symbol):
+    theBoard[spot] = symbol
 
+    for item in all_spots:
+        if item == spot:
+            all_spots.remove(item)
 
+    printBoard(theBoard)
 
-def check_result():
-    if ALL_SPOTS == []:
+def check_result(symbol):
+    if (
+            theBoard[0] == theBoard[1] == theBoard[2] == symbol or
+            theBoard[3] == theBoard[4] == theBoard[5] == symbol or
+            theBoard[6] == theBoard[7] == theBoard[8] == symbol or
+            theBoard[0] == theBoard[3] == theBoard[6] == symbol or
+            theBoard[1] == theBoard[4] == theBoard[7] == symbol or
+            theBoard[2] == theBoard[5] == theBoard[8] == symbol or
+            theBoard[0] == theBoard[4] == theBoard[8] == symbol or
+            theBoard[2] == theBoard[4] == theBoard[6] == symbol
+    ):
+
+        if symbol == "×":
+            print("You lose...")
+            # return False
+        else:
+            print("You win!")
+        return False
+
+    elif ALL_SPOTS == []:
         print("It's draw.haha")
-        return False
-
-    elif(
-            theBoard[0] == theBoard[1] == theBoard[2] == "×" or
-            theBoard[3] == theBoard[4] == theBoard[5] == "×" or
-            theBoard[6] == theBoard[7] == theBoard[8] == "×" or
-            theBoard[0] == theBoard[3] == theBoard[6] == "×" or
-            theBoard[1] == theBoard[4] == theBoard[7] == "×" or
-            theBoard[2] == theBoard[5] == theBoard[8] == "×" or
-            theBoard[0] == theBoard[4] == theBoard[8] == "×" or
-            theBoard[2] == theBoard[4] == theBoard[6] == "×" or
-            theBoard[3] == theBoard[4] == theBoard[5] == "×"
-    ):
-        print("You lose...")
-        return False
-
-    elif (
-            theBoard[0] == theBoard[1] == theBoard[2] == "○" or
-            theBoard[3] == theBoard[4] == theBoard[5] == "○" or
-            theBoard[6] == theBoard[7] == theBoard[8] == "○" or
-            theBoard[0] == theBoard[3] == theBoard[6] == "○" or
-            theBoard[1] == theBoard[4] == theBoard[7] == "○" or
-            theBoard[2] == theBoard[5] == theBoard[8] == "○" or
-            theBoard[0] == theBoard[4] == theBoard[8] == "○" or
-            theBoard[2] == theBoard[4] == theBoard[6] == "○" or
-            theBoard[3] == theBoard[4] == theBoard[5] == "○"
-    ):
-        print("You win!")
         return False
 
     else:
         pass
 
+def restart():
+    global theBoard
+    global ALL_SPOTS
+    continue_op = input("\nDo you want to try again?")
+    if continue_op.lower() == "yes":
+        theBoard = {0: '0', 1: '1', 2: '2',
+                    3: '3', 4: '4', 5: '5',
+                    6: '6', 7: '7', 8: '8'}
+        ALL_SPOTS = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+        game_status = True
+    else:
+        game_status = False
+    return game_status
 
+
+####### Game Logic #######
 game_is_on = True
 while game_is_on:
-    # Action of Computer
-    print("\nIt's computer's turn.")
-    ran_spot = random.choice(ALL_SPOTS)
-
-    theBoard[ran_spot] = "×"
-
-    for item in ALL_SPOTS:
-        if item == ran_spot:
-            ALL_SPOTS.remove(item)
-
     printBoard(theBoard)
 
-    if check_result() == False:
-        continue_op = input("\nDo you want to try again?")
-        if continue_op.lower() == "yes":
-            theBoard = {0: '0', 1: '1', 2: '2',
-                        3: '3', 4: '4', 5: '5',
-                        6: '6', 7: '7', 8: '8'}
+    # Action of Computer
+    print("\nIt's computer's turn.")
+    cp_choice = random.choice(ALL_SPOTS)
 
-            ALL_SPOTS = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    modify_board(spot=cp_choice, all_spots=ALL_SPOTS, symbol="×")
+
+    if check_result(symbol="×") == False:
+        if restart():
             continue
         else:
             game_is_on = False
@@ -84,26 +89,10 @@ while game_is_on:
     else:
         user_choice = int(input("\nIt is your turn.\nWhich left spot do you want to put your ○ ? Answer the number.\n"))
 
-        theBoard[user_choice] = "○"
+        modify_board(spot=user_choice, all_spots=ALL_SPOTS, symbol="○")
 
-        for item in ALL_SPOTS:
-            if item == user_choice:
-                ALL_SPOTS.remove(item)
-
-        printBoard(theBoard)
-
-        if check_result() == False:
-            continue_op = input("\nDo you want to try again?")
-            if continue_op.lower() == "yes":
-                theBoard = {0: '0', 1: '1', 2: '2',
-                            3: '3', 4: '4', 5: '5',
-                            6: '6', 7: '7', 8: '8'}
-
-                ALL_SPOTS = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+        if check_result(symbol="○") == False:
+            if restart():
                 continue
             else:
                 game_is_on = False
-
-   
-   
-
